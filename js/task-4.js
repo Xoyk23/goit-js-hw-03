473b777d65dcf20f42fcff547b32a19025d99e02
Ihor Prokopets <kiringa23@gmail.com>
2020-10-05 14:03:07 +0300

    add solved task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,38 @@
-const formatString = function (string, maxLength = 40) {
-  const arrayLength = string.split('');
-  if (arrayLength.length >= maxLength) {
-    arrayLength.splice(maxLength, arrayLength.length - 1, '...');
-  }
+/* Суммирование значений свойств объекта
+* Напиши функцию countTotalSalary(employees) принимающую
+* объект зарплат.Функция считает общую сумму зарплаты
+* работников и возвращает ее.Каждое поле объекта,
+передаваемого в функцию, имеет вид "имя":"зарплата". */
 
-  return arrayLength.join('');
+// 3. Перебрать массив значений и на каждой иттерацци + к сумме
+// 4. Вернуть сумму зарплаты
+
+const countTotalSalary = function (employees) {
+  ('use strict');
+  // 1. Создать переменую для общей суммы зарплат
+  let totalSalary = 0;
+  // 2. Сделать из обьекта массив значений
+  const totalSalaryArr = Object.values(employees);
+  // 3. Перебрать массив значений и на каждой иттерацци + к сумме
+  for (let salary of totalSalaryArr) {
+    totalSalary += salary;
+  }
+  return totalSalary;
 };
 
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// 'Curabitur ligula sapien, tincidunt non.'
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log('This is total salary: ', countTotalSalary(developers));
+// 1000
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// 'Vestibulum facilisis, purus nec pulvinar...'
-
-console.log(
-  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
-);
-// 'Vestibulum facilisis, purus ne...'
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log('This is total salary: ', countTotalSalary(supports));
+// 500
